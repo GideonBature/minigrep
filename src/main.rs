e3b0357127d8424c5bc6afedbc4e68b0606e38b6
GideonBature <infoaboutgideon@gmail.com>
2024-07-18 00:06:47 +0100

    refactor the args parser into a function returning a struct

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (_query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With test:\n{}", contents);
@@ -21,5 +21,5 @@ fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
     let file_path = args[2].clone();
 
-    (query, file_path)
+    Config { query, file_path }
 }
